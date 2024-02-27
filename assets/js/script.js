058594f997abeae54d8e7e94fb8c4ccaec4501e7
ShafeeqAhamedS <shafeeqahamed2804@gmail.com>
2024-02-27 20:15:47 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+var counterValue = 0
+
 const incrementFunction = () => {
+    counterValue++
+    document.getElementById('counter-number').innerText = counterValue
     console.log(`increment logged!`)
 }
 
 const decrementFunction = () => {
+    counterValue--
+    document.getElementById('counter-number').innerText = counterValue
     console.log(`decrement logged!`)
 }
